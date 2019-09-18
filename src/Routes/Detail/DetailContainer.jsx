e269b7e37ddef1d106863ad5798b1fcf8dc9594e
nmwh <43128952+do0401@users.noreply.github.com>
2019-09-18 11:01:26 +0900

    #6.8 오류 확인 중

--- src/Routes/Detail/DetailContainer.jsx
+++ src/Routes/Detail/DetailContainer.jsx
@@ -22,21 +22,22 @@ export default class extends React.Component {
 			return push('/');
 		}
 		let result = null;
-		try {
-			if (isMovie) {
-				({ data: result } = await moviesApi.movieDetail(parsedId));
-			} else {
-				({ data: result } = await tvApi.showDetail(parsedId));
-			}
-		} catch {
-			this.setState({ error: "Can't find anything."});
-		} finally {
-			this.setState({ loading: false, result });
-		}
+    try {
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parsedId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(parsedId));
+      }
+    } catch {
+      this.setState({ error: "Can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
+    }
 	}
 
 	render() {
 		const { result, error, loading } = this.state;
+		console.log(result);
 		return <DetailPresenter result={result} error={error} loaidng={loading} />;
 	}
 }
