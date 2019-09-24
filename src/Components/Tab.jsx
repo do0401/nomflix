324be724459abb0fc1ddd21f0ce430e7cb1eb857
nmwh <43128952+do0401@users.noreply.github.com>
2019-09-24 11:10:40 +0900

    #6.11 code challenges - tab styling

--- src/Components/Tab.jsx
+++ src/Components/Tab.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import styled from 'styled-components';
 
-const List = styled.ul`display: flex;`;
-
-const Item = styled.li`
-	width: 80px;
-	height: 50px;
-	text-align: center;
-	border-bottom: 5px solid ${(props) => (props.current ? '#3498db' : 'transparent')};
-	transition: border-bottom .3s ease-in-out;
-`;
-
-const SLink = styled(Link)`
-  height: 50px;
+const List = styled(TabList)`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  width: 300px;
+  justify-content: space-between;
 `;
 
-export default withRouter((props) => (
-	<List>
+// const Item = styled.li`
+// 	width: 80px;
+// 	height: 50px;
+// 	text-align: center;
+// 	border-bottom: 5px solid ${(props) => (props.current ? '#3498db' : 'transparent')};
+// 	transition: border-bottom .3s ease-in-out;
+// `;
+
+// const SLink = styled(Link)`
+//   height: 50px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
+
+export default (props) => (
+	<Tabs>
 		{console.log(props)}
-		<Item>
-			<SLink to="/"> Video </SLink>{' '}
-		</Item>{' '}
-		<Item>
-			<SLink to="/tv"> Production Companies </SLink>{' '}
-		</Item>{' '}
-		<Item>
-			<SLink to="/search"> Countries </SLink>{' '}
-		</Item>{' '}
-	</List>
-));
+		<List>
+			<Tab>Videos</Tab>
+			<Tab>Production Companies</Tab>
+			<Tab>Countries</Tab>
+		</List>
+
+		<TabPanel>
+			<h2>Videos Contents</h2>
+		</TabPanel>
+		<TabPanel>
+			<h2>Production Companies List</h2>
+		</TabPanel>
+		<TabPanel>
+			<h2>Countries List</h2>
+		</TabPanel>
+	</Tabs>
+);
